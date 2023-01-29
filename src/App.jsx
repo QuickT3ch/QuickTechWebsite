0dd7237710132660a09b34f11821845d1aa2a27c
Sarband <sarband2001muhamad@gmail.com>
2023-01-29 21:55:24 +0300

    user gesture added for the 3d logo 🥱

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,40 @@ import {Logo3D} from "./Logo3D";
 import { useEffect,useState ,useRef} from 'react';
 
 function App() {
- const [page,setPage]= useState(0);
+ const [page,setPage]= useState(1);
+ const [isRotatingLeft, setIsRotatingLeft] = useState(false)
+const [isRotatingRight, setIsRotatingRight] = useState(false)
+ const [touchStart, setTouchStart] = useState(null)
+const [touchEnd, setTouchEnd] = useState(null)
+
+// the required distance between touchStart and touchEnd to be detected as a swipe
+const minSwipeDistance = 50 
+
+const onTouchStart = (e) => {
+  setTouchEnd(null) // otherwise the swipe is fired even with usual touch events
+  setTouchStart(e.targetTouches[0].clientX)
+}
+
+const onTouchMove = (e) => setTouchEnd(e.targetTouches[0].clientX)
+
+const onTouchEnd = () => {
+  if (!touchStart || !touchEnd) return
+  const distance = touchStart - touchEnd
+  const isLeftSwipe = distance > minSwipeDistance
+  const isRightSwipe = distance < -minSwipeDistance
+  if (isLeftSwipe) {
+console.log('leftSwip');
+  }else if (isRightSwipe) {
+    console.log('rightSwip');
+  }
+  // add your conditional logic here
+}
   return (
-    <div className='h-screen w-screen bg-repeat bg-[#040A18] bg-auto bg-[url("assets/lines.svg")]'>
+    <div onTouchStart={onTouchStart} onTouchMove={onTouchMove} onTouchEnd={onTouchEnd} className='h-screen w-screen bg-repeat bg-[#040A18] bg-auto bg-[url("assets/lines.svg")]'>
     
     {/* navBar */}
     <div>
-    <img src="assets/TitleLogo.svg" alt="" srcset="" />
+    <img src="assets/TitleLogo.svg" alt=""  />
     </div>
 
     {/* 3d model  and description*/}
@@ -20,13 +47,13 @@ function App() {
         <p className='text-center w-3/4 text-3xl text-[#FBF9FD] font-[VarelaRound] capitalize'>
         Every thing is easy and fast with 
           <br /> 
-        <span className='text-[#8C1C3B]'> quick teck </span>
+        <span className='text-[#8C1C3B]'> quick tech </span>
         </p>
       </div>
       <div className='h-2/4 w-screen '>
         <Canvas>
         <pointLight castShadow intensity={5} position={[1, 20, 1]} />
-        <Logo3D />
+        <Logo3D animation={[isRotatingLeft,setIsRotatingLeft,isRotatingRight,setIsRotatingRight]}/>
         <ambientLight shadow={true}/>
         </Canvas>
       </div>
@@ -47,8 +74,13 @@ function App() {
        </p>
     </div>
     <div className='flex items-center justify-between w-32'>
-      <img src="assets/inactiveLeftButton.svg" alt="LeftButton" className='w-[34px]' />
-      <img src="assets/inactiveRightButton.svg" alt="RightButton" className='w-[34px]' />
+      <img onClick={()=>{
+        setIsRotatingLeft(!isRotatingLeft);
+      }} src="assets/inactiveLeftButton.svg" alt="LeftButton" className='w-[34px]' />
+     
+      <img onClick={()=>{
+        setIsRotatingRight(!isRotatingRight);
+      }}src="assets/inactiveRightButton.svg" alt="RightButton" className='w-[34px]' />
     </div>
     </div>
     </div>
--- src/Logo3D.jsx
+++ src/Logo3D.jsx
@@ -1,7 +1,8 @@
 
-import React, {  useRef } from 'react'
+import React, {  useEffect, useRef } from 'react'
 import { useGLTF } from '@react-three/drei'
-import { useFrame } from '@react-three/fiber';
+import { useFrame  } from '@react-three/fiber';
+
 
 export function Logo3D(props) {
 
@@ -9,17 +10,32 @@ export function Logo3D(props) {
 
   const { nodes, materials } = useGLTF('assets/3d/logo.glb')
 
-  
-    useFrame(({ clock }) => {
+  useEffect(() =>{},[props.animation[0],props.animation[2]]);
+
+    useFrame(() => {
+
       if(refMesh.current) {
-        if(refMesh.current.rotation.x < 1.5) {
-          refMesh.current.rotation.x += 0.01; 
-        }
-        if(refMesh.current.rotation.y < 0){
-          refMesh.current.rotation.y += 0.01;
-        }
-        if(refMesh.current.rotation.z < 0){
-          refMesh.current.rotation.z += 0.01;
+        console.log(refMesh.current.rotation.z);
+        if(props.animation[0] ||props.animation[2]){
+          if (props.animation[0]== true) {
+            refMesh.current.rotation.z += 0.1;
+            if (refMesh.current.rotation.z > (6.3*(1))){
+              props.animation[1](false);
+            }
+          }
+          if (props.animation[2] == true) {
+            refMesh.current.rotation.z = refMesh.current.rotation.z - 0.01;
+          }
+        }else{
+          if(refMesh.current.rotation.x < 1.5) {
+            refMesh.current.rotation.x += 0.01; 
+          }
+          if(refMesh.current.rotation.y < 0){
+            refMesh.current.rotation.y += 0.01;
+          }
+          if(refMesh.current.rotation.z < 0){
+            refMesh.current.rotation.z += 0.01;
+          }
         }
       }
     });
@@ -27,7 +43,7 @@ export function Logo3D(props) {
     
   return (
     <group castShadow  dispose={null}>
-      <group  ref={refMesh} position={[0, 0, 0]} rotation={[0,-1,-1]} scale={[35, 35, 35]}>
+      <group  ref={refMesh} position={[0, 0, 0]} rotation={[0, -1, -1]} scale={[35, 35, 35]}>
         <mesh geometry={nodes.Curve001_1.geometry} material={materials['SVGMat.003']} />
         <mesh geometry={nodes.Curve001_2.geometry} material={materials['SVGMat.003']} />
         <mesh geometry={nodes.Curve001_3.geometry} material={materials['SVGMat.003']} />
