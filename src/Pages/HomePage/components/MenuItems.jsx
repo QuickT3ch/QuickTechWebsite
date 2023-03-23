754172d0d5b3e074eeb7b5052a31e5c55cd44bc0
Ismail <Ismail.serwan727@gmail.com>
2023-03-23 22:01:38 +0300

    HamburgerMenu

--- src/Pages/HomePage/components/MenuItems.jsx
+++ src/Pages/HomePage/components/MenuItems.jsx
@@ -17,10 +17,10 @@ export default function MenuItems({showMenu,active}) {
         
         <ul className={active ? 'text-[25px] font-serif z-30 active uppercase flex flex-col items-center gap-6 justify-center inset-0 backdrop-blur-lg fixed' : 'hidden'}>
         <Close className='absolute top-10 right-10 scale-150 md:hiddens'  onClick={showMenu}/>
-        <li className='text-animation opacity-1 transition-opacity duration-500 ease-out'><FontAwesomeIcon icon={faHome}/> home</li>
-        <li className='text-animation opacity-1 transition-opacity duration-500 ease-out'><FontAwesomeIcon icon={faUser}/> Client</li>
-        <li className='text-animation opacity-1 transition-opacity duration-500 ease-out'><FontAwesomeIcon icon={faEnvelope}/> contact</li>
-        <li className='text-animation opacity-1 transition-opacity duration-500 ease-out'><FontAwesomeIcon icon={faInfoCircle}/> about</li>
+        <li><FontAwesomeIcon icon={faHome}/> home</li>
+        <li><FontAwesomeIcon icon={faUser}/> Client</li>
+        <li><FontAwesomeIcon icon={faEnvelope}/> contact</li>
+        <li><FontAwesomeIcon icon={faInfoCircle}/> about</li>
         </ul>
         </div>
        
